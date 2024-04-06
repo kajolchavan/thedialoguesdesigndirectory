8fdf0e55c6bebdd4fab7d6349b115fc9a2a638c8
kajolchavan <projectmanagement.kajol@gmail.com>
2024-04-06 17:55:53 +0530

    final final changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce7d5f4bc37f23ee"; }
+/******/ 		__webpack_require__.h = function() { return "32d0b548f51c797b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.a488b0ecc0ed3d26.hot-update.js
+++ .next/static/webpack/webpack.a488b0ecc0ed3d26.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8c82d5558c669e54"; }
+/******/ 	__webpack_require__.h = function() { return "c86245e200c8f487"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
